b85ab6345f3c0b0461455f320cce54ccaab876f8
s-jafari <shv.jafari@gmail.com>
2017-07-10 11:51:53 +0200

    models assignment current state

---
+++ spec/models/question_spec.rb
@@ -0,0 +1,9 @@
+RSpec.describe Question do
+  let(:question) { Question.create!(title: "New Question Title", body: 'Question Body', resolved: false) }
+
+  describe "attributes" do
+    it "has a title, body and resolved attribute" do
+      expect(question).to have_attributes(title: "New Question Title", body: 'Question Body', resolved: false)
+    end
+  end
+end
