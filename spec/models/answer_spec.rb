29944004075bf6aefe927c4a0e63ce41d5863faf
s-jafari <shv.jafari@gmail.com>
2017-07-11 22:14:39 +0200

    model error correction

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe Answer do
   let(:question) { Question.new(title: "New Question Title", body: 'Question Body', resolved: false) }
-  let(:answer) { Answer.new( body: 'Answer Body' ) }
+  let(:answer) { Answer.new( body: 'Answer Body', question: question ) }
 
   describe "attributes" do
     it "has a body" do
