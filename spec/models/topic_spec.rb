153d03af1e879ca06ba33e25d0ba84bceff408c2
s-jafari <shervin@Shervins-MacBook.local>
2017-07-30 12:43:11 +0200

    topics checkpoint

---
+++ spec/models/topic_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+
+RSpec.describe Topic, type: :model do
+  let(:name) { RandomData.random_sentence }
+  let(:description) { RandomData.random_paragraph }
+  let(:public) { true }
+  let(:topic) { Topic.create!(name: name, description: description) }
+
+  it { is_expected.to have_many(:posts) }
+
+  describe "attributes" do
+    it "has name, description, and public attributes" do
+      expect(topic).to have_attributes(name: name, description: description, public: public)
+    end
+
+    it "is public by default" do
+      expect(topic.public).to be(true)
+    end
+  end
+
+  describe "GET show" do
+    it "returns http success" do
+      get :show, {id: my_topic.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, {id: my_topic.id}
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_topic to @topic" do
+      get :show, {id: my_topic.id}
+      expect(assigns(:topic)).to eq(my_topic)
+    end
+  end
+end
