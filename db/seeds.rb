9d296f2e6bc456fcdade0a929e7b71a8ae1ad511
s-jafari <shervin@Shervins-MacBook.local>
2017-07-31 14:27:31 +0200

    sponsored posts

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,24 @@ require 'random_data'
 
 15.times do
   Topic.create!(
-    name:         RandomData.random_sentence,
-    description:  RandomData.random_paragraph
+    name: RandomData.random_sentence,
+    description: RandomData.random_paragraph
   )
 end
 topics = Topic.all
 
 50.times do
+  SponsoredPost.create!(
+    topic: topics.sample,
+    title: RandomData.random_sentence,
+    body: RandomData.random_paragraph,
+    price: rand(1..100)
+  )
+end
+
+50.times do
   Post.create!(
-    topic:  topics.sample,
+    topic: topics.sample,
     title: RandomData.random_sentence,
     body: RandomData.random_paragraph
   )
@@ -26,5 +35,6 @@ end
 
 puts "Seed finished"
 puts "#{Topic.count} topics created"
+puts "#{SponsoredPost.count} sponsored posts created"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
