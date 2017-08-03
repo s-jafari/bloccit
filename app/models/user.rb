bc043736bbfd6f4d42d14f9736d558462303d3d0
s-jafari <shervin@Shervins-MacBook.local>
2017-08-03 11:53:46 +0200

    user name formatting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,14 @@
 class User < ActiveRecord::Base
   before_save { self.email = email.downcase if email.present? }
+  before_save {
+    if name
+      name_array = name.split
+      name_array.each do |word|
+        word.capitalize!
+      end
+      self.name = name_array.join(" ")
+    end
+  }
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: "password_digest.nil?"
@@ -9,6 +18,5 @@ class User < ActiveRecord::Base
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
 
-            # #6
-            has_secure_password
+  has_secure_password
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,6 +19,12 @@ RSpec.describe User, type: :model do
     it "should have name and email attributes" do
       expect(user).to have_attributes(name: "Bloccit User", email: "user@bloccit.com")
     end
+
+    it "should properly format the username" do
+      user.name = "test user"
+      user.save
+      expect(user.name).to eq("Test User")
+    end
   end
 
   describe "invalid user" do
