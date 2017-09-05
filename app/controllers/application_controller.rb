9efd819dcc1578eb00585eee0b13b083c4ccf975
s-jafari <shervin@shervinsmacbook.lan>
2017-09-05 13:09:20 +0200

    user authorization checkpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,7 +9,6 @@ class ApplicationController < ActionController::Base
   def require_sign_in
     unless current_user
       flash[:alert] = "You must be logged in to do that"
-      # #11
       redirect_to new_session_path
     end
   end
