277cac9740f0b84c9aa9851517cc2b5978f813d0
s-jafari <shervin@Shervins-MacBook.local>
2017-09-25 19:48:12 +0100

    voting checkpoint

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170908115547) do
+ActiveRecord::Schema.define(version: 20170925183419) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20170908115547) do
     t.datetime "updated_at", null: false
     t.integer  "topic_id"
     t.integer  "user_id"
+    t.float    "rank"
   end
 
   add_index "posts", ["topic_id"], name: "index_posts_on_topic_id"
@@ -71,5 +72,16 @@ ActiveRecord::Schema.define(version: 20170908115547) do
     t.datetime "updated_at",      null: false
     t.integer  "role"
   end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer  "value"
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "votes", [nil], name: "index_votes_on_post"
+  add_index "votes", [nil], name: "index_votes_on_user"
 
 end
