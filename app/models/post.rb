3b865e623cbdca85e2159c91f99446d60ae36c05
s-jafari <shervin@ShervinsMacBook.home>
2017-09-28 23:22:21 +0100

    voting assignment

--- app/models/post.rb
+++ app/models/post.rb
@@ -11,6 +11,7 @@ class Post < ActiveRecord::Base
   validates :topic, presence: true
   validates :user, presence: true
 
+  after_create :create_vote
 
   def up_votes
     votes.where(value: 1).count
@@ -28,5 +29,11 @@ class Post < ActiveRecord::Base
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def create_vote
+    user.votes.create(value: 1, post: self)
   end
 end
